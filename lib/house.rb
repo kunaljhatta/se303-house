40c7f1e0985f6a0156f1481808ae1596b8f85ee4
KunalJhatta <Kunaljhatta22@gmail.com>
2022-04-19 10:19:23 -0700

    pass test_line_3 and refactored test_line_2 test

--- lib/house.rb
+++ lib/house.rb
@@ -3,8 +3,10 @@ class House
         case number
         when 1
             "This is the house that Jack built.\n"
-        else
-            "This is the malt that lay in the house that Jack built.\n"
+        when 2
+            "This is the malt that lay in the house that Jack built.\n" 
+        when 3
+            "This is the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 end
