6041b513934c73715e815555a34437439adab749
KunalJhatta <Kunaljhatta22@gmail.com>
2022-05-29 21:18:12 -0700

    replaced segment in the 7th case with call to the method

--- lib/house.rb
+++ lib/house.rb
@@ -26,7 +26,7 @@ class House
     def line(number)
         case number
         when 7
-            "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is #{segment(number)}the house that Jack built.\n"
         when 8
             "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 9
