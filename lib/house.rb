2403930c5e1a32293bdaacf769b61847f64819f4
KunalJhatta <Kunaljhatta22@gmail.com>
2022-05-29 22:12:35 -0700

    replaced the cases with the recursive call in the segment method

--- lib/house.rb
+++ lib/house.rb
@@ -1,39 +1,45 @@
 class House
-
+  
     def recite 
         (1..12).each.map {|n| line(n)}.join("\n")
     end 
 
     def segment(number)
-        case number
-        when 1
-          ""
-        when 2
-            "the malt that lay in "
-        when 3
-            "the rat that ate the malt that lay in "
-        when 4
-            "the cat that killed the rat that ate the malt that lay in "
-        when 5
-            "the dog that worried the cat that killed the rat that ate the malt that lay in "
-        when 6
-            "the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
-        when 7
-            "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
-        when 8
-            "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
-        when 9
-            "the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
-        when 10
-            "the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
-        when 11
-            "the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
-        when 12
-            "the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
-        end
+      case number
+      when 1
+        "#{verses[number-1]}"
+      when 2
+        "the malt that lay in #{segment(number-1)}"
+      when 3
+        "the rat that ate #{segment(number-1)}"
+      when 4
+        "the cat that killed #{segment(number-1)}"
+      when 5
+        "the dog that worried #{segment(number-1)}"
+      when 6
+        "the cow with the crumpled horn that tossed #{segment(number-1)}"
+      when 7
+        "the maiden all forlorn that milked #{segment(number-1)}"
+      when 8
+        "the man all tattered and torn that kissed #{segment(number-1)}"
+      when 9
+        "the priest all shaven and shorn that married #{segment(number-1)}"
+      when 10
+        "the rooster that crowed in the morn that woke #{segment(number-1)}"
+      when 11
+        "the farmer sowing his corn that kept #{segment(number-1)}"
+      when 12
+        "the horse and the hound and the horn that belonged to #{segment(number-1)}"
+      end
     end
 
     def line(number)
-            "This is #{segment(number)}the house that Jack built.\n"
+        "This is #{segment(number)}"
+      end  
+  
+    def verses
+      [
+        "the house that Jack built.\n",
+      ]
     end
-end
+  end
