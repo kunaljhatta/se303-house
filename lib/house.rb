cd8ec1f209a6f57c86944e744fe743b5f3f6c0b8
KunalJhatta <Kunaljhatta22@gmail.com>
2022-05-29 21:33:15 -0700

    Added 11th segment to segment method

--- lib/house.rb
+++ lib/house.rb
@@ -26,6 +26,8 @@ class House
             "the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         when 10
             "the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+        when 11
+            "the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         end
     end
 
