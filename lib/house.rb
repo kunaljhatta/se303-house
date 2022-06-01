0159bc951d983fb30c0367e9c03cf0d367d5a27b
KunalJhatta <Kunaljhatta22@gmail.com>
2022-05-31 20:46:43 -0700

    Remove all when cases and change case to an if statement and add the last part of the song to verses

--- lib/house.rb
+++ lib/house.rb
@@ -4,57 +4,32 @@ class House
         (1..12).each.map {|n| line(n)}.join("\n")
     end 
 
+    def line(number)
+      "This is #{segment(number)}"
+    end
+  
     def segment(number)
-      case number
-      when 0
+      if number == 0
         ""
-      when 1
-        "#{verses[number-1]}"
-      when 2
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 3
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 4
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 5
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 6
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 7
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 8
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 9
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 10
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 11
-        "#{verses[number-1]}#{segment(number-1)}"
-      when 12
-        "#{verses[number-1]}#{segment(number-1)}"
       else
-        "This is #{segment(number)}the house that Jack built.\n"
+        "#{verses[number-1]}#{segment(number-1)}"
       end
     end
-
-    def line(number)
-        "This is #{segment(number)}the house that Jack built.\n"
-      end  
   
     def verses
-        [
-          "",
-          "the malt that lay in ",
-          "the rat that ate ",
-          "the cat that killed ",
-          "the dog that worried ",
-          "the cow with the crumpled horn that tossed ",
-          "the maiden all forlorn that milked ",
-          "the man all tattered and torn that kissed ",
-          "the priest all shaven and shorn that married ",
-          "the rooster that crowed in the morn that woke ",
-          "the farmer sowing his corn that kept ",
-          "the horse and the hound and the horn that belonged to "
-        ]
+      [
+        "the house that Jack built.\n",
+        "the malt that lay in ",
+        "the rat that ate ",
+        "the cat that killed ",
+        "the dog that worried ",
+        "the cow with the crumpled horn that tossed ",
+        "the maiden all forlorn that milked ",
+        "the man all tattered and torn that kissed ",
+        "the priest all shaven and shorn that married ",
+        "the rooster that crowed in the morn that woke ",
+        "the farmer sowing his corn that kept ",
+        "the horse and the hound and the horn that belonged to "
+      ]
     end
   end
